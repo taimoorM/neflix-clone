ff5fc45a473219d08a55fe0ddd9ee22f6874971f
Taimoor M <taimoor.m89@gmail.com>
2021-02-08 16:38:52 -0500

    - added global styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "normalize.css";
 import App from "./app";
-import GlobalStyles from "./global-styles";
+import { GlobalStyles } from "./global-styles";
 
 ReactDOM.render(
   <>
