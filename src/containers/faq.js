47309be83714d495ea8ccb52558850e3852a29bf
Taimoor M <taimoor.m89@gmail.com>
2021-02-09 17:05:11 -0500

    - added CTA form component

--- src/containers/faq.js
+++ src/containers/faq.js
@@ -1,5 +1,6 @@
 import faqData from "../fixtures/faqs.json";
 import { Accordian } from "../components";
+import OptForm from "../components/optForm";
 
 export function FaqsContainer() {
   return (
@@ -11,6 +12,14 @@ export function FaqsContainer() {
           <Accordian.Body>{item.body}</Accordian.Body>
         </Accordian.Item>
       ))}
+
+      <OptForm>
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership.
+        </OptForm.Text>
+        <OptForm.Input placeholder="Email address" />
+        <OptForm.Button>GET STARTED</OptForm.Button>
+      </OptForm>
     </Accordian>
   );
 }
