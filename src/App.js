5b3f037de44088d04e8ef872f9d5faa286d1512a
Taimoor M <taimoor.m89@gmail.com>
2021-02-08 21:12:36 -0500

    - added footer component container and content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { FooterContainer } from "./containers/footer";
 import { JumbotronContainer } from "./containers/jumbotron";
 
 function App() {
-  return <JumbotronContainer />;
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
+  );
 }
 
 export default App;
--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -2,11 +2,10 @@ import styled from "styled-components/macro";
 
 export const Container = styled.div`
   display: flex;
-  padding: 70px 56px;
+  padding: 70px 0;
   margin: auto;
   max-width: 1000px;
   flex-direction: column;
-
   @media (max-width: 1000px) {
     padding: 70px 30px;
   }
@@ -20,11 +19,10 @@ export const Column = styled.div`
 
 export const Row = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(210px, 1fr));
   grid-gap: 15px;
-
   @media (max-width: 1000px) {
-    grid-template-columns: repeat(auto-fill, minmax (150px 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
   }
 `;
 
@@ -47,7 +45,7 @@ export const Text = styled.p`
   margin-bottom: 40px;
 `;
 
-export const Break = styled.p`
+export const Break = styled.div`
   flex-basis: 100%;
   height: 0;
 `;
