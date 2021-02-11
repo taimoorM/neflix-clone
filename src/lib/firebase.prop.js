1567964ef3178f3fd83e9430ebcfa6eead00656a
Taimoor M <taimoor.m89@gmail.com>
2021-02-10 20:05:56 -0500

    - added firebase boilerplate

---
+++ src/lib/firebase.prop.js
@@ -0,0 +1,12 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+//seed Db
+
+//config
+const config = {};
+
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };
