c003159d798295fa3cb71182b6dbe3e5c5953be9
Taimoor M <taimoor.m89@gmail.com>
2021-02-12 14:57:56 -0500

    - added form for signup page with header and footer

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,13 @@
 import { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
 import { FirebaseContext } from "../context/firebase";
 import { FooterContainer } from "../containers/footer";
 import { HeaderContainer } from "../containers/header";
 import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 
 export default function Login() {
+  const history = useHistory();
   const { firebase } = useContext(FirebaseContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -22,6 +25,7 @@ export default function Login() {
       .signInWithEmailAndPassword(email, password)
       .then(() => {
         //Go to browse page
+        history.push(ROUTES.BROWSE);
       })
       .catch((e) => {
         setEmail("");
@@ -54,7 +58,7 @@ export default function Login() {
             </Form.Submit>
           </Form.Base>
           <Form.Text>
-            New to Netflix? <Form.Link>Sign Up</Form.Link>
+            New to Netflix? <Form.Link to={ROUTES.SIGN_UP}>Sign Up</Form.Link>
           </Form.Text>
           <Form.TextSmall>
             This page is protected by Google reCAPTCHA to ensure you're not a
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,3 +1,79 @@
+import { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
+import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
+
 export default function Signup() {
-  return <p>Hey this is the sign up page!</p>;
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = firstName === "" || password === "" || email === "";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //firebase auth
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        //Go to browse page
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((e) => {
+        setEmail("");
+        setPassword("");
+        setError(e.message);
+      });
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title> Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSubmit} method="POST">
+            <Form.Input
+              placeholder="First name"
+              type="text"
+              value={firstName}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <Form.Input
+              placeholder="Email address"
+              type="email"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <Form.Input
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign Up
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            Already a user? <Form.Link to={ROUTES.LOGIN}>Sign In</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
