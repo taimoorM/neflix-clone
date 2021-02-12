2f6669e2808fad4591d0ec32f851130cdbec94b8
Taimoor M <taimoor.m89@gmail.com>
2021-02-11 19:01:00 -0500

    - component structure and state to login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,41 @@
+import { useState } from "react";
 import { FooterContainer } from "../containers/footer";
 import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  // check form input
+  const isInvalid = password === "" || email === "";
+
   return (
     <>
-      <HeaderContainer>Hey this is the login page!</HeaderContainer>
+      <HeaderContainer>
+        <Form>
+          <Form.Title> Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSubmit} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              type="email"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <Form.Input
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
       <FooterContainer />
     </>
   );
